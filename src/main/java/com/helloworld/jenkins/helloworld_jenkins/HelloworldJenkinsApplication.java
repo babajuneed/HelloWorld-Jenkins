8f485d29f7f010a3e3814d9765b7f1c421410e0e
Atharsh S <atharshsivasamy99@gmail.com>
2024-10-27 15:11:58 +0530

    Added empty rest controller for hello world

--- src/main/java/com/helloworld/jenkins/helloworld_jenkins/HelloworldJenkinsApplication.java
+++ src/main/java/com/helloworld/jenkins/helloworld_jenkins/HelloworldJenkinsApplication.java
@@ -2,12 +2,19 @@ package com.helloworld.jenkins.helloworld_jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloworldJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldJenkinsApplication.class, args);
 	}
 
+	@GetMapping
+	public String getHelloWorld() {
+		return "Hello World! Spring Boot application has been running successfully";
+	}
 }
