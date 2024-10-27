84559745ca230e9d8f314766c8101bbcf46470fc
Atharsh S <atharshsivasamy99@gmail.com>
2024-10-27 15:09:17 +0530

    Spring boot intialized

---
+++ src/main/java/com/helloworld/jenkins/helloworld_jenkins/HelloworldJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.jenkins.helloworld_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/jenkins/helloworld_jenkins/HelloworldJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.jenkins.helloworld_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
